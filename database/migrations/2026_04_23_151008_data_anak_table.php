72770d485e9d1cef9df741dbc208b9e9ecb0ea87
RZ2HERO <109345773+RZ2HERO@users.noreply.github.com>
2026-04-23 22:33:30 +0700

    add migrate data_anak

---
+++ database/migrations/2026_04_23_151008_data_anak_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        Schema::create('data_anak', function (Blueprint $table){
+            $table->id('idAnak_Panti');
+            $table->char('NIK', 16)->unique();
+            $table->string('nama', 45);
+            $table->date('tgl_lahir'); 
+            $table->enum('jns_kelamin', ['L', 'P']); 
+            $table->string('alamat', 45)->nuillable(); 
+            $table->date('tgl_masuk'); 
+            $table->enum('status', ['Aktif', 'Alumni', 'Pindah'])->default('Aktif'); 
+             
+            $table->string('nama_Ortu', 45)->nullable(); 
+            $table->string('Foto', 50)->nullable(); 
+            $table->enum('kategori_anak', ['Yatim', 'Piatu', 'Yatim Piatu', 'Dhuafa']);
+            $table->timestamps('created_at');
+            $table->timestamps('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+        Schema::dropIfExist('data_anak');
+    }
+};
