07dc51a59234b83365192fcecb124b530665ebbc
RZ2HERO <109345773+RZ2HERO@users.noreply.github.com>
2026-04-25 14:58:47 +0700

    add template view donatur, anakPanti, & Pengurus dan add controller template DOnatur

---
+++ app/Http/Controllers/DonaturController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DonaturController extends Controller
+{
+    //
+    public function index(){
+
+    }
+
+    public function create()
+    {
+        return view('donatur.create');
+    }
+
+    public function store(){
+        $request->validate([
+       
+        ]);
+    }
+
+   public function show(){ }
+
+   public function edit(){
+
+    }
+
+ 
+    public function update(){ }
+    public function destroy(){ }
+
+}
